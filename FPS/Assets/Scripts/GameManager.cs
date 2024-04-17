718596a962bccbb4a77e388f3acdd162fab8841e
MaverickSpore <mattmanweaver@gmail.com>
2024-04-17 00:51:57 -0400

    GameManager Update
    - Includes 4 Spawners
      - One for each type
    - Will be updated using pushes from between this commit and the previous

--- FPS/Assets/Scripts/GameManager.cs
+++ FPS/Assets/Scripts/GameManager.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject assaultSpawner;
+    [SerializeField] GameObject ninjaSpawner;
+    [SerializeField] GameObject sniperSpawner;
+    [SerializeField] GameObject reaperSpawner;
+
     public static GameManager instance;
+
+    [SerializeField] GameObject assaultEnemy;
+    [SerializeField] GameObject ninjaEnemy;
+    [SerializeField] GameObject sniperEnemy;
+    [SerializeField] GameObject reaperEnemy;
+
     public GameObject Player;
 
 
 
     public bool isPaused;
+    int playerType;
     int enemyCount;
     // Start is called before the first frame update
     void Awake()
@@ -47,5 +60,47 @@ public class GameManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         //menuActive.SetActive(isPaused);
         //menuActive = null;
+    }
+    void SpawnPlayers()
+    {
+        switch (playerType)
+        {
+            case 0:
+                SpawnNinja();
+                SpawnSniper();
+                SpawnReaper();
+                break;
+            case 1:
+                SpawnAssault();
+                SpawnSniper();
+                SpawnReaper();
+                break;
+            case 2:
+                SpawnAssault();
+                SpawnNinja();
+                SpawnReaper();
+                break;
+            case 3:
+                SpawnAssault();
+                SpawnNinja();
+                SpawnSniper();
+                break;
+        }
+    }
+    void SpawnAssault()
+    {
+        Instantiate(assaultEnemy, assaultSpawner.transform.position, transform.rotation);
+    }
+    void SpawnNinja()
+    {
+        Instantiate(ninjaEnemy, assaultSpawner.transform.position, transform.rotation);
+    }
+    void SpawnSniper()
+    {
+        Instantiate(sniperEnemy, assaultSpawner.transform.position, transform.rotation);
+    }
+    void SpawnReaper()
+    {
+        Instantiate(reaperEnemy, assaultSpawner.transform.position, transform.rotation);
     }
 }
