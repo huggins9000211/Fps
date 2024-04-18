1669d554c6efff8266cd31ac5bd2f55981259b74
King1David <137794685+King1David@users.noreply.github.com>
2024-04-18 00:22:18 -0400

    Dj-playerSelectUpdate
    fixed errors
    added text to player select options
    removed code from level
    saved everthing working ?

--- FPS/Assets/Scripts/LevelPrefab.cs
+++ FPS/Assets/Scripts/LevelPrefab.cs
@@ -5,32 +5,16 @@ using UnityEngine;
 
 public class LevelPrefab : MonoBehaviour
 {
-    public GameObject[] _characterArr;
-    public GameObject _instantiatePoint = null;
+    
 
-
-
-    void Awake()
-    {
-        foreach (GameObject i in _characterArr)
-        {
-            i.GetComponent<Rigidbody>().isKinematic = false;
-            i.GetComponent<Rigidbody>().useGravity = true;
-        }
-
-        if (_instantiatePoint == null)
-            _instantiatePoint = GameObject.FindGameObjectWithTag("InstantiatePoint").gameObject;
-    }
-
+    // Start is called before the first frame update
     void Start()
     {
-        GameObject playerCharacter = Instantiate(_characterArr[PlayerPrefs.GetInt("selectedCharacterInt")],_instantiatePoint.transform.position,_instantiatePoint.transform.rotation);
-        playerCharacter.name = "Player";
+       
     }
 
 
   
-    // Start is called before the first frame update
     
 
     // Update is called once per frame
