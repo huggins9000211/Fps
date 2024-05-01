51de55ed8faf80a22cd83c6d40f383abd8208b8a
MaverickSpore <mattmanweaver@gmail.com>
2024-05-01 17:49:00 -0400

    Explosion
    - Added explosion to grenade

--- FPS/Assets/Scripts/AudioManager.cs
+++ FPS/Assets/Scripts/AudioManager.cs
@@ -4,40 +4,4 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [Header("----- Components -----")]
-    [SerializeField] AudioSource aud;
-
-    [Header("----- Audio -----")]
-    [SerializeField] AudioClip[] audJump;
-    [Range(0, 1)][SerializeField] float audJumpVol;
-
-    [SerializeField] AudioClip[] audHurt;
-    [Range(0, 1)][SerializeField] float audHurtVol;
-
-    [SerializeField] AudioClip[] audSteps;
-    [Range(0, 1)][SerializeField] float audStepsVol;
-
-
-
-
-    public void PlayJump()
-    {
-        aud.PlayOneShot(audJump[Random.Range(0, audJump.Length)], audJumpVol);
-    }
-    public void PlayHurt()
-    {
-        aud.PlayOneShot(audHurt[Random.Range(0, audHurt.Length)], audHurtVol);
-    }
-    public IEnumerator PlaySteps()
-    {
-        GameManager.instance.basePlayer.playingSteps = true;
-        aud.PlayOneShot(audSteps[Random.Range(0, audSteps.Length)], audStepsVol);
-
-        if (!GameManager.instance.basePlayer.isSprinting)
-            yield return new WaitForSeconds(0.5f);
-        else
-            yield return new WaitForSeconds(0.3f);
-
-        GameManager.instance.basePlayer.playingSteps = false;
-    }
 }
